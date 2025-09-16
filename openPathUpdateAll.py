8e6d05ed93ea33090c2394ac615e12fef79c79b8
Cam <camherringshaw@gmail.com>
2025-09-16 07:17:28 -0500

    Add MembershipStats class instead of separate variables

--- openPathUpdateAll.py
+++ openPathUpdateAll.py
@@ -15,6 +15,22 @@ logging.basicConfig(
          level=logging.INFO,
          datefmt='%Y-%m-%d %H:%M:%S')
 
+class MembershipStats:
+    def __init__(self):
+        self.subscriberCount = 0
+        self.ceramicsCount = 0
+        self.facilityUserCount = 0
+        self.ceramicsFacilityCount = 0
+        self.paidRegulars = 0
+        self.paidCeramics = 0
+
+        self.warningUsers = []
+        self.missingTourSubscribers = {}
+        self.missingWaiverSubscribers = {}
+        self.missingCsiSubscribers = {}
+        self.compedSubscribers = []
+        self.compedLeaders = []
+
 def getWarningText(warningUsers):
     if len(warningUsers) == 0:
         return ""
@@ -26,50 +42,36 @@ def getWarningText(warningUsers):
 
 def openPathUpdateAll(neonAccounts, mailSummary = False):
     opUsers = openPathUtil.getAllUsers()
-
-    subscriberCount = 0
-    ceramicsCount = 0
-    facilityUserCount = 0
-    ceramicsFacilityCount = 0
-
-    warningUsers = []
-    missingTourSubscribers = {}
-    missingWaiverSubscribers = {}
-    missingCsiSubscribers = {}
-    compedSubscribers = []
-    compedLeaders = []
-
-    paidRegulars = 0
-    paidCeramics = 0
+    stats = MembershipStats()
 
     for account in neonAccounts:
         if not neonAccounts[account].get("paidRegular") and not neonAccounts[account].get("paidCeramics") and not neonUtil.accountIsType(neonAccounts[account], neonUtil.STAFF_TYPE):
             #Accounts that are neither paid nor staff might still have access
             if neonUtil.accountIsType(neonAccounts[account], neonUtil.LEAD_TYPE):
-                compedLeaders.append(f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")})''')
+                stats.compedLeaders.append(f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")})''')
             elif neonAccounts[account].get("compedRegular") or neonAccounts[account].get("compedCeramics"):
-                compedSubscribers.append(f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")})''')
+                stats.compedSubscribers.append(f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")})''')
 
         if neonAccounts[account].get("validMembership"):
-            subscriberCount += 1
+            stats.subscriberCount += 1
             if neonAccounts[account].get("ceramicsMembership"):
-                ceramicsCount += 1
+                stats.ceramicsCount += 1
 
             #accounts with concurrent paid regular and ceramics memberships are most likely
             #upgrades that should be only counted as ceramics members
             if neonAccounts[account].get("paidCeramics"):
-                paidCeramics += 1
+                stats.paidCeramics += 1
             elif neonAccounts[account].get("paidRegular"):
-                paidRegulars += 1
+                stats.paidRegulars += 1
 
         if neonAccounts[account].get("paidRegular") and neonAccounts[account].get("paidCeramics"):
             logging.info(f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")}) has concurrent paid memberships.''')
 
         if neonUtil.subscriberHasFacilityAccess(neonAccounts[account]):
-            facilityUserCount += 1
+            stats.facilityUserCount += 1
 
         if neonUtil.subscriberHasCeramicsAccess(neonAccounts[account]):
-            ceramicsFacilityCount += 1
+            stats.ceramics_facility_count += 1
 
         if neonAccounts[account].get("OpenPathID"):
             openPathUtil.updateGroups(neonAccounts[account], 
@@ -77,7 +79,7 @@ def openPathUpdateAll(neonAccounts, mailSummary = False):
             #note that this isn't necessarily 100% accurate, because we have Neon users with provisioned OpenPath IDs and no access groups
             #assuming that typical users who gained and lost openPath access have a signed waiver
             if not neonAccounts[account].get("WaiverDate"):
-                warningUsers.append(f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")})''')
+                stats.warningUsers.append(f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")})''')
         elif neonUtil.accountHasFacilityAccess(neonAccounts[account]):
             neonAccounts[account] = openPathUtil.createUser(neonAccounts[account])
             openPathUtil.updateGroups(neonAccounts[account],
@@ -86,43 +88,43 @@ def openPathUpdateAll(neonAccounts, mailSummary = False):
         elif neonAccounts[account].get("validMembership"):
             startDate = neonAccounts[account].get("Membership Start Date")
             if not neonAccounts[account].get("WaiverDate"):
-                missingWaiverSubscribers[account] = f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")}) - since {startDate}'''
+                stats.missingWaiverSubscribers[account] = f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")}) - since {startDate}'''
             if not neonAccounts[account].get("FacilityTourDate"):
-                missingTourSubscribers[account] = f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")}) - since {startDate}'''
+                stats.missingTourSubscribers[account] = f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")}) - since {startDate}'''
 
         #an account might be missing CSI but still have regular facility access stuff handled
         if neonAccounts[account].get("ceramicsMembership") and not neonAccounts[account].get("CsiDate"):
-            missingCsiSubscribers[account] = f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")}) - since {neonAccounts[account].get("Ceramics Start Date")}'''
+            stats.missingCsiSubscribers[account] = f'''{neonAccounts[account].get("fullName")} ({neonAccounts[account].get("Email 1")}) - since {neonAccounts[account].get("Ceramics Start Date")}'''
 
     list_separator = '\n            '
     compedSubscriberString = ""
     compedSubscriberDetails =""
     compedLeaderDetails = ""
-    if (len(compedSubscribers)+len(compedLeaders) > 0):
-        compedSubscriberString = f''' plus {len(compedSubscribers)+len(compedLeaders)} complimentary memberships'''
-        if (len(compedLeaders) > 0):
+    if (len(stats.compedSubscribers)+len(stats.compedLeaders) > 0):
+        compedSubscriberString = f''' plus {len(stats.compedSubscribers)+len(stats.compedLeaders)} complimentary memberships'''
+        if (len(stats.compedLeaders) > 0):
             compedLeaderDetails = f'''
         Comped Volunteer Leaders:
-            {list_separator.join(compedLeaders[x] for x in range(len(compedLeaders)))}
+            {list_separator.join(stats.compedLeaders[x] for x in range(len(stats.compedLeaders)))}
 '''
-        if (len(compedSubscribers) > 0):
+        if (len(stats.compedSubscribers) > 0):
             compedSubscriberDetails = f'''
         Other Comped Memberships:
-            {list_separator.join(compedSubscribers[x] for x in range(len(compedSubscribers)))}
+            {list_separator.join(stats.compedSubscribers[x] for x in range(len(stats.compedSubscribers)))}
 '''
 
     msg = MIMEText(f'''
-    Today Asmbly has {paidRegulars} regular and {paidCeramics} ceramics subscribers{compedSubscriberString}.
+    Today Asmbly has {stats.paidRegulars} regular and {stats.paidCeramics} ceramics subscribers{compedSubscriberString}.
 
     Of those:
-        {facilityUserCount} have facility access and {ceramicsFacilityCount} have ceramics access
-        {len(missingWaiverSubscribers)} are missing the waiver{':' if len(missingWaiverSubscribers) > 0 else ' (yay!)'}
-            {list_separator.join(missingWaiverSubscribers[x] for x in sorted(missingWaiverSubscribers, reverse=True))}
-        {len(missingTourSubscribers)} are missing orientation{':' if len(missingTourSubscribers) > 0 else ' (yay!)'}
-            {list_separator.join(missingTourSubscribers[x] for x in sorted(missingTourSubscribers, reverse=True))}
-        {len(missingCsiSubscribers)} are missing Ceramics Introduction{':' if len(missingCsiSubscribers) > 0 else ' (yay!)'}
-            {list_separator.join(missingCsiSubscribers[x] for x in sorted(missingCsiSubscribers, reverse=True))}
-{getWarningText(warningUsers)}
+        {stats.facilityUserCount} have facility access and {stats.ceramicsFacilityCount} have ceramics access
+        {len(stats.missingWaiverSubscribers)} are missing the waiver{':' if len(stats.missingWaiverSubscribers) > 0 else ' (yay!)'}
+            {list_separator.join(stats.missingWaiverSubscribers[x] for x in sorted(stats.missingWaiverSubscribers, reverse=True))}
+        {len(stats.missingTourSubscribers)} are missing orientation{':' if len(stats.missingTourSubscribers) > 0 else ' (yay!)'}
+            {list_separator.join(stats.missingTourSubscribers[x] for x in sorted(stats.missingTourSubscribers, reverse=True))}
+        {len(stats.missingCsiSubscribers)} are missing Ceramics Introduction{':' if len(stats.missingCsiSubscribers) > 0 else ' (yay!)'}
+            {list_separator.join(stats.missingCsiSubscribers[x] for x in sorted(stats.missingCsiSubscribers, reverse=True))}
+{getWarningText(stats.warningUsers)}
 {compedLeaderDetails}
 {compedSubscriberDetails}
 {commonMessageFooter}
@@ -131,16 +133,16 @@ def openPathUpdateAll(neonAccounts, mailSummary = False):
     msg['Subject'] = "Asmbly Daily Subscriber Update"
 
     summaryMsg = MIMEText(f'''
-    Today Asmbly has {paidRegulars} regular and {paidCeramics} ceramics paid subscribers.
+    Today Asmbly has {stats.paidRegulars} regular and {stats.paidCeramics} ceramics paid subscribers.
 
     Additionally:
-        {len(compedLeaders)} Asmbly leaders have free memberships.
-        {len(compedSubscribers)} other Asmbly volunteers have free memberships.
+        {len(stats.compedLeaders)} Asmbly leaders have free memberships.
+        {len(stats.compedSubscribers)} other Asmbly volunteers have free memberships.
 
-    {facilityUserCount} users have facility access and {ceramicsFacilityCount} have ceramics access
-    {len(missingWaiverSubscribers)} are missing the waiver.
-    {len(missingTourSubscribers)} are missing orientation.
-    {len(missingCsiSubscribers)} are missing Ceramics Introduction.
+    {stats.facilityUserCount} users have facility access and {stats.ceramicsFacilityCount} have ceramics access
+    {len(stats.missingWaiverSubscribers)} are missing the waiver.
+    {len(stats.missingTourSubscribers)} are missing orientation.
+    {len(stats.missingCsiSubscribers)} are missing Ceramics Introduction.
 
 {commonMessageFooter}
 ''')
